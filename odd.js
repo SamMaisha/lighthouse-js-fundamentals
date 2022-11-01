74ec5edcf707e3227432c921cf73983e74df00ef
Sam Maisha <sam.maisha99@gmail.com>
2022-11-01 11:40:04 +0000

    odd checker

---
+++ odd.js
@@ -0,0 +1,13 @@
+function isOdd(oddNum) {
+  if (oddNum % 2 === 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+console.log("10975 is odd: " + isOdd(10975));
+
+
